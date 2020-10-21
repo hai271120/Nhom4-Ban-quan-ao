75b8b0a24b225a1967871501eb74e28d0abd2af2
dung1202 <70959545+dung1202@users.noreply.github.com>
2020-10-21 21:10:08 +0700

    Add files via upload

--- du lieu.js
+++ du lieu.js
@@ -1,41 +1,41 @@
 let sp = [
     {
-        name: 'aoNamSweater',
+        name: 'Ao Nam Sweater',
         price: 299e3,
         img: ['https://product.hstatic.net/1000299894/product/1.2.04.2.02.007.219.01.10200011___10__0630ea13500843c4a01096e3933dcff8_1024x1024.jpg',
             'https://product.hstatic.net/1000299894/product/1.2.04.2.02.007.219.01.10200011___8__eadacb7270e34dd99de4c43aa55de2c2_1024x1024.jpg'
         ],
     },
     {
-        name: 'aoNuHoodie',
+        name: 'Ao Nu Hoodie',
         price: 349e3,
         img: ['https://product.hstatic.net/1000299894/product/1.0.06.2.02.002.219.01.10100011___5__92d19f8216c34eb4a53d0c9657c26c33_1024x1024.jpg',
             'https://product.hstatic.net/1000299894/product/1.0.06.2.02.002.219.01.10100011___2__5bba1d0674b74150a8e46d4640b605e4_1024x1024.jpg'
         ],
     },
     {
-        name: 'aoNamCoat',
+        name: 'Ao Nam Coat',
         price: 499e3,
         img: ['https://product.hstatic.net/1000299894/product/1.2.05.2.05.003.219.01.10800030___2__0f9a37d11b7c412aa4546ecbc3c789dc_1024x1024.jpg',
             'https://product.hstatic.net/1000299894/product/1.2.05.2.05.003.219.01.10800030___3__e3dd813a114c4aa4823aa421526672ab_1024x1024.jpg'
         ],
     },
     {
-        name: 'quanNuJean',
+        name: 'Quan Nu Jean',
         price: 249e3,
         img: ['https://product.hstatic.net/1000299894/product/31d448d1-dafd-4fd2-adac-f31ef598cf70_b5faebfb1ad74f2cac89b6a0452d08f4_1024x1024.jpeg',
             'https://product.hstatic.net/1000299894/product/31d448d1-dafd-4fd2-adac-f31ef598cf70_b5faebfb1ad74f2cac89b6a0452d08f4_1024x1024.jpeg'
         ],
     },
     {
-        name: 'quanNamKakiBaggy',
+        name: 'Quan Nam KakiBaggy',
         price: 199e3,
         img: ['https://product.hstatic.net/1000299894/product/1.2.22.2.14.002.120.23.10700023___1__7b6aecfca48a4400a565244c813d2748_1024x1024.jpg',
             'https://product.hstatic.net/1000299894/product/1.2.22.2.14.002.120.23.10700023___3__95e3a49b16894a46b21f6dfc85e56dc5_1024x1024.jpg'
         ],
     },
     {
-        name: 'quanNuJogger',
+        name: 'Quan Nu Jogger',
         price: 249e3,
         img: ['https://product.hstatic.net/1000299894/product/1.2.24.1.05.002.219.01__14__2585b103e4994e5bb0b29c029c17db35_1024x1024.jpg',
             'https://product.hstatic.net/1000299894/product/1.2.24.1.05.002.219.01__16__dd843dff1cf14cd2b8e71b9ebb4772a2_1024x1024.jpg  '
@@ -90,25 +90,73 @@ function enterLogin(e, t) {
     if (e.key === 'Enter') data();
 }
 
-// function dataNam() {
-//     let a = []
-//     for (let i = 0; i < sp.length; i++) {
-//         const kt = sp[i];
-//         if (kt.name.toLowerCase().indexOf('nam') !== -1) a.push(kt);
-//     }
-//     displaylistphone(a);
-// }
+function dataNam() {
+    let a = []
+    for (let i = 0; i < sp.length; i++) {
+        const kt = sp[i];
+        if (kt.name.toLowerCase().indexOf('nam') !== -1) a.push(kt);
+    }
+    displaylistphone(a);
+}
 
-// function dataNu() {
-//     let a = []
-//     for (let i = 0; i < sp.length; i++) {
-//         const kt = sp[i];
-//         if (kt.name.toLowerCase().indexOf('nu') !== -1) a.push(kt);
-//     }
-//     displaylistphone(a);
-// }
+function dataNu() {
+    let a = []
+    for (let i = 0; i < sp.length; i++) {
+        const kt = sp[i];
+        if (kt.name.toLowerCase().indexOf('nu') !== -1) a.push(kt);
+    }
+    displaylistphone(a);
+}
 function viewDetail(name) {
     localStorage.setItem('selected', name);
     localStorage.setItem('products', JSON.stringify(sp));
+    localStorage.setItem('mang', JSON.stringify(gioHang))
     window.location.href = 'lua chon.html';
+}
+
+
+let w = localStorage.getItem('k')
+console.log(w)
+if (w !== null) {
+    q();
+}
+function q() {
+    if (w === 'Nam') {
+        dataNam();
+        k();
+    }
+    else if (w === 'Nu') {
+        dataNu();
+        k();
+    }
+    else {
+        dataq(w);
+        k();
+    }
+}
+
+function k()
+{
+    w = null
+    localStorage.setItem('k', null)
+}
+
+function dataq(b)
+{
+    let a=[];
+    let key=b;
+    let q=key;
+    if (key < 1000) { q = key * 1000 }
+    if (key === '') {
+        return;
+    }
+    else {
+        for (let i = 0; i < sp.length; i++) {
+            const kt = sp[i];
+            if (kt.price >= q) a.push(kt);
+            else if (kt.name.toLowerCase().indexOf(key.toLowerCase()) !== -1) a.push(kt);
+        }
+        if (a.length === 0) alert('khong tim thay yeu cau, moi ban nhap lai')
+        else displaylistphone(a);
+    }
 }
--- du lieu2.js
+++ du lieu2.js
@@ -6,39 +6,100 @@ let product = products.find(function (p) {
     return p.name === selectedName;
 });
 
+function enterLogin(e, t) {
+    if (e.key === 'Enter') dataName();
+}
 function displaylistphone(phones) {
+    // tiều đề bảng
+    let kt= document.getElementById('tieude')
+    kt.innerHTML=
+                `<td style="text-align: center">STT</td>
+                <td>Tùy chỉnh</td>
+                <td>Tên sản phẩm</td>
+                <td style="text-align: center">Ảnh</td>
+                <td style="text-align: center">Giá tiền</td>
+                <td style="text-align: center">Số lượng</td>
+                <td style="text-align: center">Thành tiền</td>`
+
+    // nội dung bảng
     let listphones = document.getElementById('list-phone')
     listphones.innerHTML = ''; // xóa mảng trc khi thêm
     for (let i = 0; i < phones.length; i++) {
         const phoneName = phones[i];
         listphones.innerHTML +=
-            `<div>
-            <div>${phoneName.name}</div>
-            <div><h4>${phoneName.price}đ (x${phoneName.sl})</h4></div>
-            <div><img class="" src="${phoneName['img'][0]}"></div>
-            <div><button><a onclick="xoa(i)" href="gio hang.html"> xoa </a></button></div>
-            </div>`
+            `<tr>
+            <td><div style="text-align: center">${i + 1}</div></td>
+            <td><div><button onclick="xoa(${i})">Xóa</button></div><div><button onclick="sua(${i})">Sửa</button></div></td>
+            <td>${phoneName.name}</td>
+            <td><img style="text-align: center" width="40px" height="50px" class="" src="${phoneName['img'][0]}"></td>
+            <td style="text-align: right">${phoneName.price}đ</td>
+            <td style="text-align: right">x${phoneName.sl}</td>
+            <td style="text-align: right">${phoneName.price * phoneName.sl}đ</td>
+            <tr>
+            `
     }
-}
 
-displaylistphone(t)
-console.log(t);
+    // tổng kết
+    let tien = 0;
+    for (let i = 0; i < t.length; i++) {
+        tien = tien + (t[i]['price'] * t[i]['sl'])
+    }
+    let tinh = document.getElementById('tien')
+    tinh.innerHTML =
+        `
+    <td colspan="6" style="text-align: center">TỔNG</td>
+    <td style="text-align: right">${tien}đ</td>`
+
+}
+if (localStorage.getItem('mang') === '[]') {
+    let kt = document.getElementById('kt')
+    kt.innerHTML = `
+    <tr>
+    <td>Bạn chưa chọn sản phẩm</td>
+    </tr>`
+}
+else {
+    displaylistphone(t)
+}
 function xoa(a) {
     t.splice(a, 1);
-    viewDetail(t);
+    viewDetail();
+}
+const luongHang = document.getElementById('luonghang')
+luongHang.innerText = t.length
+
+function sua(a)
+{
+    kt = t[a]['name']
+    viewDetail1(kt)
 }
 
-let tien = 0;
-for (i = 0; i > t.length; i++) {
-    tien = tien + (t[0]['price']*t[0]['sl'])
-    console.log(tien)
-}
-let tong = document.getElementById('tong')
-tong.innerHTML = tien +'đ'
-
-function viewDetail(t) {
+function viewDetail() {
     localStorage.setItem('mang', JSON.stringify(t));
     window.location.href = 'gio hang.html';
 }
 
+function viewDetail1(a)
+{
+    localStorage.setItem('selected', a)
+    window.location.href = 'lua chon.html'
+}
 
+function dataNam()
+{
+    localStorage.setItem('k', 'Nam')
+    window.location.href = 'cua hang quan ao.html'
+}
+
+function dataNu()
+{
+    localStorage.setItem('k', 'Nu')
+    window.location.href = 'cua hang quan ao.html'
+}
+
+function dataName()
+{
+    let key = document.getElementById('search')
+    localStorage.setItem('k', key.value.trim())
+    window.location.href = 'cua hang quan ao.html'
+}
